df2cb32e9bc1a3e75b3139f5ff34c276ef7dc54a
Alexey Kudryavtsev <westor@me.com>
2025-02-01 16:50:08 +0600

    Rewrite library page using masonry

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -5,43 +5,41 @@ import {Text} from "../../atoms/text";
 import {strings} from "../../i18n/i18n.ts";
 import {SimpleCardSmall} from "../../components/cards/simpleCardSmall";
 import ArrowButton from "../../atoms/arrowButton";
-import {LINKS} from "../../constants/strings.ts";
+import {MasonryLayoutWrapper} from "../../templates/masonryLayoutWrapper";
+import {presets} from "../../constants/presets.ts";
 
 export const LibraryPage: FC = () => {
+
     return (
         <AnyPageWrapper>
-            <Text text={strings.libraryPage.talksHeader} size={"h1"} color={"primary"}></Text>
+            <div className={styles.mainContainer}>
+                <MasonryLayoutWrapper
+                    desktopColumnsCount={3}
+                    header={<Text text={strings.libraryPage.talksHeader} size={"h1"} color={"primary"}></Text>}
+                    footer={<ArrowButton {...presets.calendlySignUp} />}
+                >
+                    {Object.values(strings.libraryPage.talks).map(({title, source, url}) => (
+                        <SimpleCardSmall
+                            label={source}
+                            title={title}
+                            onTitleClick={() => window.open(url)}
+                        ></SimpleCardSmall>
+                    ))}
+                </MasonryLayoutWrapper>
 
-            <div className={styles.contentContainer}>
-                {Object.values(strings.libraryPage.talks).map(({title, source, url}) => (
-                    <SimpleCardSmall
-                        label={source}
-                        title={title}
-                        onTitleClick={() => window.open(url)}
-                    ></SimpleCardSmall>
-                ))}
-            </div>
-
-            <div className={styles.signUpContainer}>
-                <ArrowButton text={strings.header.signUp} onClick={() => window.open(LINKS.CALENDLY)}/>
-            </div>
-
-            <div className={styles.articlesHeader}>
-                <Text text={strings.libraryPage.articlesHeader} size={"h1"} color={"primary"}></Text>
-            </div>
-
-            <div className={styles.contentContainer}>
-                {Object.values(strings.libraryPage.articles).map(({title, source, url}) => (
-                    <SimpleCardSmall
-                        label={source}
-                        title={title}
-                        onTitleClick={() => window.open(url)}
-                    ></SimpleCardSmall>
-                ))}
-            </div>
-
-            <div className={styles.signUpContainer}>
-                <ArrowButton text={strings.header.signUp} onClick={() => window.open(LINKS.CALENDLY)}/>
+                <MasonryLayoutWrapper
+                    desktopColumnsCount={3}
+                    header={<Text text={strings.libraryPage.articlesHeader} size={"h1"} color={"primary"}></Text>}
+                    footer={<ArrowButton {...presets.calendlySignUp} />}
+                >
+                    {Object.values(strings.libraryPage.articles).map(({title, source, url}) => (
+                        <SimpleCardSmall
+                            label={source}
+                            title={title}
+                            onTitleClick={() => window.open(url)}
+                        ></SimpleCardSmall>
+                    ))}
+                </MasonryLayoutWrapper>
             </div>
         </AnyPageWrapper>
 
